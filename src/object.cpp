7280a6b8bfd9affd594bdc311c1ec10f1aed2f3f
wilimitis <wilimitis9@gmail.com>
2018-12-29 01:35:54 -0600

    Implement ray world space transform.

--- src/object.cpp
+++ src/object.cpp
@@ -8,6 +8,28 @@ Object::Object()
   localTransformation = glm::mat4(1);
 }
 
+Ray Object::getLocal(const Ray &ray)
+{
+  glm::mat4 worldTransformation = glm::inverse(localTransformation);
+
+  // Ignore the translation component for directions.
+  // Shirley 6.3
+  glm::vec3 localPosition = worldTransformation * glm::vec4(ray.position, 1);
+  glm::vec3 localDirection = worldTransformation * glm::vec4(ray.direction, 0);
+
+  return Ray(localDirection, localPosition);
+}
+
+Ray Object::getWorld(const Ray &ray)
+{
+  // Ignore the translation component for directions.
+  // Shirley 6.3
+  glm::vec3 worldPosition = localTransformation * glm::vec4(ray.position, 1);
+  glm::vec3 worldDirection = localTransformation * glm::vec4(ray.direction, 0);
+
+  return Ray(worldDirection, worldPosition);
+}
+
 void Object::rotate(float angle, const glm::vec3 &axis)
 {
   localTransformation = glm::rotate(localTransformation, angle, axis);
@@ -21,22 +43,4 @@ void Object::scale(const glm::vec3 &scale)
 void Object::translate(const glm::vec3 &translation)
 {
   localTransformation = glm::translate(localTransformation, translation);
-}
-
-Ray Object::getLocal(const Ray &ray)
-{
-  glm::mat4 inverseTransformation = glm::inverse(localTransformation);
-  glm::vec3 position = glm::vec3(localTransformation[3]);
-
-  // Ignore the translation component for directions.
-  // Shirley 6.3
-  glm::vec3 localPosition = inverseTransformation * glm::vec4(ray.position, 1);
-  glm::vec3 localDirection = inverseTransformation * glm::vec4(ray.direction, 0);
-
-  return Ray(localDirection, localPosition);
-}
-
-Ray Object::getWorld(const Ray &ray)
-{
-  // TODO
 }
--- src/object.h
+++ src/object.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "glm/glm.hpp"
+#include "hit.h"
 #include "ray.h"
 
 class Object
@@ -8,14 +9,13 @@ class Object
   public:
     glm::mat4 localTransformation;
 
-    virtual bool intersects(const Ray &ray) = 0;
+    virtual Hit intersects(const Ray &ray) = 0;
 
     Object();
 
+    Ray getLocal(const Ray &ray);
+    Ray getWorld(const Ray &ray);
     void rotate(float angle, const glm::vec3 &axis);
     void scale(const glm::vec3 &scale);
     void translate(const glm::vec3 &translation);
-
-    Ray getLocal(const Ray &ray);
-    Ray getWorld(const Ray &ray);
 };
